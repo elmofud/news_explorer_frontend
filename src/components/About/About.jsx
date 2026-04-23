81c3e1fc2bdb4c448a7787b3dd52e9f63bb737bf
Tracey Lee <traceylg43@gmail.com>
2026-04-23 13:04:13 -0400

    Style About section, fix header position issue

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,4 +1,4 @@
-import avatar from "../../images/avatar-space.png";
+import avatar from "../../images/placeholder-image.svg";
 
 import "./About.css";
 
@@ -6,13 +6,15 @@ const About = () => {
   return (
     <div className="about">
       <img src={avatar} className="about__avatar-image" alt="avatar image" />
-      <h2 className="about__title">About the author</h2>
-      <p className="about__description">
-        This block describes the project author. Here you should indicate your
-        name, what you do, and which development technologies you know. You can
-        also talk about your experience with TripleTen, what you learned there,
-        and how you can help potential customers.
-      </p>
+      <div className="about__container">
+        <h2 className="about__title">About the author</h2>
+        <p className="about__ifo">
+          This block describes the project author. Here you should indicate your
+          name, what you do, and which development technologies you know. You
+          can also talk about your experience with TripleTen, what you learned
+          there, and how you can help potential customers.
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,18 +1,22 @@
 import heroBackground from "../../images/bg-hero.svg";
+import About from "../About/About.jsx";
 import SearchForm from "../SearchForm/SearchForm.jsx";
 
 import "./Hero.css";
 
 const Hero = () => {
   return (
-    <section className="hero">
-      <h1 className="hero__title">What's going on in the world?</h1>
-      <p className="hero__subtitle">
-        Find the latest news on any topic and save them in your personal
-        account.
-      </p>
-      <SearchForm />
-    </section>
+    <>
+      <section className="hero">
+        <h1 className="hero__title">What's going on in the world?</h1>
+        <p className="hero__subtitle">
+          Find the latest news on any topic and save them in your personal
+          account.
+        </p>
+        <SearchForm />
+      </section>
+      <About />
+    </>
   );
 };
 
