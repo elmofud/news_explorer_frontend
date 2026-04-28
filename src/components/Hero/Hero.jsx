2d7e6700ee751c6648c8085c8909b4983e04e605
Tracey Lee <traceylg43@gmail.com>
2026-04-28 16:56:33 -0400

    WIP: hero broken, save before charge dies

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -3,18 +3,23 @@ import SearchForm from "../SearchForm/SearchForm.jsx";
 import "./Hero.css";
 
 const Hero = ({ onSearch }) => {
-  return (
-    <>
-      <section className="hero">
-        <h1 className="hero__title">What's going on in the world?</h1>
-        <p className="hero__subtitle">
-          Find the latest news on any topic and save them in your personal
-          account.
-        </p>
-        <SearchForm onSearch={onSearch} />
-      </section>
-    </>
-  );
+    return (
+        <>
+            <section className="hero">
+                <div className="hero__text">
+                    <h1 className="hero__title">
+                        What's going on in the world?
+                    </h1>
+                    <p className="hero__subtitle">
+                        Find the latest news on any topic and save them in your
+                        personal account.
+                    </p>
+                </div>
+
+                <SearchForm onSearch={onSearch} />
+            </section>
+        </>
+    );
 };
 
 export default Hero;
--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -2,31 +2,31 @@ import { useState } from "react";
 import "./SearchForm.css";
 
 const SearchForm = ({ onSearch }) => {
-  const [query, setQuery] = useState("");
+    const [query, setQuery] = useState("");
 
-  const handleChange = (evt) => setQuery(evt.target.value);
+    const handleChange = (evt) => setQuery(evt.target.value);
 
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    if (query.trim() === "") return;
-    onSearch(query);
-  };
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        if (query.trim() === "") return;
+        onSearch(query);
+    };
 
-  return (
-    <form className="search-form" onSubmit={handleSubmit}>
-      <input
-        required
-        className="search-form__input"
-        type="text"
-        placeholder="Text not entered"
-        value={query}
-        onChange={handleChange}
-      />
-      <button className="search-form__button" type="submit">
-        Search
-      </button>
-    </form>
-  );
+    return (
+        <form className="search-form" onSubmit={handleSubmit}>
+            <input
+                required
+                className="search-form__input"
+                type="text"
+                placeholder="Text not entered"
+                value={query}
+                onChange={handleChange}
+            />
+            <button className="search-form__button" type="submit">
+                Search
+            </button>
+        </form>
+    );
 };
 
 export default SearchForm;
