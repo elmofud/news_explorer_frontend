ae118e380182f9db8937355750a6642301706cdb
Tracey Lee <traceylg43@gmail.com>
2026-05-01 08:44:58 -0400

    refactor: consolidate assets into images folder and update impors

--- src/components/RegisterModal/RegisterModal.jsx
+++ src/components/RegisterModal/RegisterModal.jsx
@@ -2,69 +2,69 @@ import ModalWithForm from "../ModalWithForm/ModalWithForm.jsx";
 import "./RegisterModal.css";
 
 const RegisterModal = ({
-  isOpen,
-  onClose,
-  buttonText,
-  onLoginClick,
-  onSignupSuccess,
+    isOpen,
+    onClose,
+    buttonText,
+    onLoginClick,
+    onSignupSuccess,
 }) => {
-  const handleSubmit = (evt) => {
-    evt.preventDefault();
-    onSignupSuccess();
-  };
-  return (
-    <ModalWithForm
-      isOpen={isOpen}
-      title="Sign up"
-      onClose={onClose}
-      onSubmit={handleSubmit}
-      buttonText={buttonText}
-      alternateButton={
-        <button
-          type="button"
-          className="modal__alternate-button"
-          onClick={onLoginClick}
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        onSignupSuccess();
+    };
+    return (
+        <ModalWithForm
+            isOpen={isOpen}
+            title="Sign up"
+            onClose={onClose}
+            onSubmit={handleSubmit}
+            buttonText={buttonText}
+            alternateButton={
+                <button
+                    type="button"
+                    className="modal__alternate-button"
+                    onClick={onLoginClick}
+                >
+                    or{" "}
+                    <span className="modal__alternate-button-link">Log In</span>
+                </button>
+            }
         >
-          or <span className="modal__alternate-button-link">Log In</span>
-        </button>
-      }
-    >
-      <label htmlFor="register-email" className="modal__label">
-        Email{" "}
-        <input
-          required
-          type="email"
-          className="modal__input"
-          id="register-email"
-          placeholder="Enter email"
-          name="email"
-          // value={values.email}
-        />
-      </label>
-      <label htmlFor="register-password" className="modal__label">
-        Password{" "}
-        <input
-          required
-          type="password"
-          className="modal__input"
-          id="register-password"
-          placeholder="Enter password"
-          name="password"
-        />
-      </label>
-      <label htmlFor="register-username" className="modal__label">
-        Username{" "}
-        <input
-          required
-          type="text"
-          className="modal__input"
-          id="register-username"
-          placeholder="Enter your username"
-          name="username"
-        />
-      </label>
-    </ModalWithForm>
-  );
+            <label htmlFor="register-email" className="modal__label">
+                Email{" "}
+                <input
+                    required
+                    type="email"
+                    className="modal__input"
+                    id="register-email"
+                    placeholder="Enter email"
+                    name="email"
+                />
+            </label>
+            <label htmlFor="register-password" className="modal__label">
+                Password{" "}
+                <input
+                    required
+                    type="password"
+                    className="modal__input"
+                    id="register-password"
+                    placeholder="Enter password"
+                    name="password"
+                />
+            </label>
+            <label htmlFor="register-username" className="modal__label">
+                Username{" "}
+                <input
+                    required
+                    type="text"
+                    className="modal__input"
+                    id="register-username"
+                    placeholder="Enter your username"
+                    name="username"
+                />
+            </label>
+        </ModalWithForm>
+    );
 };
 
 export default RegisterModal;
